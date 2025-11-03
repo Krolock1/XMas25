14066ce73c7833d939b66e337dd6c3a9e8f5389e
Markus Walgenbach <m.walgenbach@neusta.de>
2025-11-03 09:52:16 +0100

    add Kinderpunsch and Herrengedeck

--- src/components/CalculatorView.vue
+++ src/components/CalculatorView.vue
@@ -2,16 +2,16 @@
     <div class="p-3 text-center flex flex-col gap-3 bg-green-100 text-2xl min-h-screen">
         <h2 class="font-bold"><img src="/pwa-192-192.png" alt="Glühweinrechner 2025" class="inline-block w-10 h-10 mr-2" />Glühweinrechner 2025</h2>
         <div 
-        class="grid grid-cols-2 md:grid-cols-4 gap-2 auto-rows-fr"        
+        class="grid grid-cols-2 md:grid-cols-3 gap-2 auto-rows-fr"        
         >
             <div 
             v-for="item in xConfig" 
             :key="item.id" 
-            class="border p-2 rounded-xl flex flex-col gap-2 text-center justify-center items-center"
+            class="border p-2 rounded-xl flex flex-col gap-1 text-center justify-center items-center text-stone-900"
             :class="item.background"
             @click="item.count++"
             >
-                <div class="font-extrabold text-xl flex flex-wrap gap-1 items-center justify-center">
+                <div class="font-bold text-xl flex flex-wrap gap-0 items-center justify-center">
                     <span v-for="(name, index) in item.displayNames" :key="name">{{ name }}<template v-if="index < item.displayNames.length - 1">, </template></span>
                 </div>
                 <div class="grow"></div>
@@ -33,7 +33,7 @@
         <div class="rounded-xl bg-red-600 text-white p-2" @click="clear">
             Zurücksetzen
         </div>
-        <div class="text-xs text-gray-600 h-20 mb-1">
+        <div class="text-xs text-gray-600 mb-1 h-16">
             © 2025 Markus Walgenbach
         </div>        
     </div>
@@ -53,26 +53,42 @@ interface XItem {
 const xConfig: Ref<XItem[]> = ref(      [
     {
         id: 'main-drinks',
-        background: 'bg-pink-600',
-        displayNames: ['Glühwein', 'Punsch Schuss', 'Bier', 'Cola', 'Apfelschorle'],
+        background: 'bg-red-600',
+        displayNames: ['Glühwein', 'Bier','Punsch Schuss', 'Softgetränk'],
         amount: 3,
         deposit: 2,
         count:0,
     },
     {
-        id: 'special-drinks',
-        background: 'bg-amber-600',
-        displayNames: ['Glühwein Schuss'],
-        amount: 3.5,
+        id: 'child-drink',
+        background: 'bg-amber-500',
+        displayNames: ['Kinderpunsch'],
+        amount: 2.5,
         deposit: 2,
         count:0,
     },
     {
-        id: 'water-shnaps',
+        id: 'water-schnaps',
         background: 'bg-blue-300',
         displayNames: ['Wasser', 'Schnaps'],
-        amount: 4,
+        amount: 2,
         deposit: 2,
+        count:0,
+    },
+    {
+        id: 'special-drink',
+        background: 'bg-red-400',
+        displayNames: ['Glühwein mit Schuss'],
+        amount: 3.5,
+        deposit: 4,
+        count:0,
+    },
+    {
+        id: 'man-drink',
+        background: 'bg-amber-600',
+        displayNames: ['Herrengedeck'],
+        amount: 4,
+        deposit: 4,
         count:0,
     },
     {
